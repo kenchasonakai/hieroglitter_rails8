c84533f0774ba8fa70d0baaa4f797a8555b0d50e
kenchasonakai <kenchasonakai@gmail.com>
2024-12-08 00:58:00 +0900

    投稿時に画像が保存されるように設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,12 +5,37 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(body: params[:post][:body].to_hieroglyph)
+    @post = Post.new(post_params)
     if @post.save
       redirect_to posts_path
     else
       @posts = Post.all
       render :index
+    end
+  end
+
+  private
+
+  def post_params
+    { body: hierogliph_body, image: attach_ogp_image }
+  end
+
+  def attach_ogp_image
+    build_ogp_image(hierogliph_body)
+  end
+
+  def hierogliph_body
+    params[:post][:body].to_hieroglyph
+  end
+
+  def build_ogp_image(text)
+    image = MiniMagick::Image.open(Rails.root.join('app', 'assets', 'images', 'ogp.png'))
+    image.combine_options do |config|
+      config.font Rails.root.join('app', 'assets', 'fonts', 'SegoeUiHistoric.ttf')
+      config.fill 'white'
+      config.gravity 'center'
+      config.pointsize 65
+      config.draw "text 0, 0 '#{text.scan(/.{1,13}/).join("\n")}'"
     end
   end
 end
