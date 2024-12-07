8a1bb52ccd9b79875fbf15209957db0fd80f80b3
kenchasonakai <kenchasonakai@gmail.com>
2024-12-08 01:55:11 +0900

    fix: rubocop

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,10 @@
 CarrierWave.configure do |config|
-
   config.fog_credentials = {
-    provider:              'AWS',
+    provider:              "AWS",
     aws_access_key_id:     Rails.application.credentials.aws[:access_key_id],
     aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-    region: 'ap-northeast-1',
+    region: "ap-northeast-1"
   }
-  config.fog_directory  = 'hieroglitter-images'
+  config.fog_directory  = "hieroglitter-images"
   config.fog_public = false
 end
