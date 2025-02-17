963881a30c17299bdceb93c2de16ff251e1e2056
kenchasonakai <kenchasonakai@gmail.com>
2025-02-17 23:10:20 +0900

    add: 認証認可用のコントローラーを追加

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  allow_unauthenticated_access only: %i[ create ]
+  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
+
+  def create
+    user_ids = User.pluck(:id)
+    redirect_to root_path, error: '𓃭𓍯𓎼𓇋𓈖 𓆑𓄿𓇋𓃭𓇋𓂧' if user_ids.blank?
+
+    user = User.find(user_ids.sample)
+    start_new_session_for user
+    redirect_to posts_path, success: "𓃭𓍯𓎼𓇋𓈖 𓆑𓄿𓇋𓃭𓇋𓂧"
+  end
+
+  def destroy
+    terminate_session
+    redirect_to root_path
+  end
+end
--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,6 @@
 class TopsController < ApplicationController
+  allow_unauthenticated_access only: %i[ index ]
+
   def index
     post = Post.find_by(id: params[:post])
     set_meta_tags(og: { image: post.image&.url }) if post
