75fa0ec4e4ce1e46fc240f69d7dd091d13518fec
kenchasonakai <kenchasonakai@gmail.com>
2025-02-17 23:04:29 +0900

    add: User用のテーブルを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.create(name: "𓎡𓃭𓇋𓍯𓏤𓄿𓏏𓂋𓄿") # cleopatra
+User.create(name: "𓎡𓎛𓈖𓅱𓅓 𓎡𓎛𓅱𓆑𓅱") # world_giza_sphynx
+User.create(name: "𓏏𓅱𓏏𓄿𓈖𓎡𓎛𓄿𓅓𓅱𓈖") # Tut_Ankh_Amon_tsutankamen
+User.create(name: "𓄿𓅓𓇋𓈖𓎛𓍯𓏏𓇋𓏤") # cat_sphynx
+User.create(name: "𓂧𓄿𓂋𓇋𓇋𓍯𓋴") # pyramid
