d91eee1f9e790a8ab57c634bff566e092c4f36ea
kenchasonakai <kenchasonakai@gmail.com>
2025-04-12 16:31:58 +0900

    add: 翻訳されたテキストの読み上げ機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,7 @@ Rails.application.routes.draw do
   resources :posts, only: %i[index create]
   resources :favorites, only: %i[create]
   resources :translate_hieroglyphs, only: %i[create]
+  namespace :api do
+    resources :translated_texts, only: %i[show]
+  end
 end
